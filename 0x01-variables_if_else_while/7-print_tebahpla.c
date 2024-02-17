9679d83ca72a7c0c91b8d06fd9df2c608268a58a
smakrix <smakrix@gmail.com>
2024-02-17 06:11:24 -0800

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This will print all alphabet in reverse
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
