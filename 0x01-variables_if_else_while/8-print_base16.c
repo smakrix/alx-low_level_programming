d2e21667288949eaee50365bd9d89b1d4884d59c
smakrix <smakrix@gmail.com>
2024-02-17 06:41:52 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,10 @@ int main(void)
 	char alphabet;
 	int number;
 
-	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
-		putchar(alphabet);
 	for (number = 0; number < 10; number++)
 		putchar((number % 10) + '0');
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+		putchar(alphabet);
 
 	putchar('\n');
 
