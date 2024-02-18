2a1f1f9569c4500c18871f6cd31c946a7f4a031e
smakrix <smakrix@gmail.com>
2024-02-18 12:41:13 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,21 +3,20 @@
 /**
  * main - this will rints the numbers from 00 to 99, numbers separated by
  *        a comma followed by a space, in ascending order
- *
  * Return: Always 0.
  */
 int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 9 && num2 == 9)
+			if (num1 == 8 && num2 == 9)
 				continue;
 			
 			putchar(',');
